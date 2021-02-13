356d9baad4816d01f3b2322f9c696ba883b0956d
leonardovee <leoveeph@gmail.com>
2021-02-13 14:43:34 -0300

    refactor: make sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,25 +2,43 @@ import { LogControllerDecorator } from './log'
 import { Controller } from '../../presentation/protocols'
 import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
 
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          email: 'any_email@mail.com',
+          name: 'any_name',
+          password: 'any_password',
+          confirmPasword: 'any_password'
+        }
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  const controllerStub: ControllerStub = new ControllerStub()
+  return controllerStub
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub: Controller = makeController()
+  const sut: LogControllerDecorator = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            email: 'any_email@mail.com',
-            name: 'any_name',
-            password: 'any_password',
-            confirmPasword: 'any_password'
-          }
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub: ControllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut: LogControllerDecorator = new LogControllerDecorator(controllerStub)
     const httpRequest: HttpRequest = {
       body: {
         email: 'any_email@mail.com',
