62abd102f632bf7841b50702414afba1fc611271
leonardovee <leoveeph@gmail.com>
2021-03-07 00:05:11 -0300

    feat: add LoginValidationFactory

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,11 +1,11 @@
-import { Controller } from '../../presentation/protocols'
-import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
 import { makeSignUpValidation } from './signup-validation'
-import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
-import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
-import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 
 export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
