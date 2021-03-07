8084e9df5923c519496e202f58155c4ae01eedcb
leonardovee <leoveeph@gmail.com>
2021-03-07 12:47:11 -0300

    refactor: move interfaces to its own folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,7 +2,7 @@ import { LogControllerDecorator } from './log'
 import { Controller } from '../../presentation/protocols'
 import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 
 const makeServerError = (): HttpResponse => {
