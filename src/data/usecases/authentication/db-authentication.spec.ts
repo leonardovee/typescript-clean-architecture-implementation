03a199baf2e674aa88c7910bb4943d63ddac37fb
leonardovee <leoveeph@gmail.com>
2021-03-07 12:29:24 -0300

    refactor: move fake account creation to a helper method

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -2,16 +2,18 @@ import { DbAuthentication } from './db-authentication'
 import { AccountModel } from '../add-account/db-add-account-protocols'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 describe('DbAuthentication UseCase', () => {
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
     class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
       async load (email: string): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email@mail.com',
-          password: 'any_password'
-        }
+        const account: AccountModel = makeFakeAccount()
         return new Promise(resolve => resolve(account))
       }
     }
