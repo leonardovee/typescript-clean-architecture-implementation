4b4e9676f5e554c71f9220f9430f6358aa85ba75
leonardovee <leoveeph@gmail.com>
2021-03-14 21:38:41 -0300

    feat: add login routes

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -10,8 +10,9 @@ import { Controller } from '../../../presentation/protocols'
 import env from '../../config/env'
 
 export const makeLoginController = (): Controller => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
   const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  const bcryptAdapter = new BcryptAdapter(12)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
   const loginController = new LoginController(dbAuthentication, makeLoginValidation())
