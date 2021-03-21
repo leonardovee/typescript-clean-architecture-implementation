fa07379f01a9aca5afb9f1c03fb234dfb84cdf88
leonardovee <leonardovee@hotmail.com>
2021-03-20 23:51:15 -0300

    refactor: put errors in line

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -17,18 +17,14 @@ export class SignUpController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
-      if (error) {
-        return badRequest(error)
-      }
+      if (error) return badRequest(error)
       const { name, email, password } = httpRequest.body
       const account = await this.addAccount.add({
         name,
         email,
         password
       })
-      if (!account) {
-        return forbidden(new EmailAlreadyTakenError())
-      }
+      if (!account) return forbidden(new EmailAlreadyTakenError())
       const accessToken = await this.authentication.auth({
         email,
         password
