480c6dd687a55b7b70f0a689a48d59a217e6816a
leonardovee <leoveeph@gmail.com>
2021-04-05 22:22:01 -0300

    refactor: move surcey factories to its own folder

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,5 +1,5 @@
-import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
 import { Router } from 'express'
 
 export default (router: Router): void => {
