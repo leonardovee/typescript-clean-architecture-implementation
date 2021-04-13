9e2020bf49c6ef773de5ddf594fcc2f076d554c4
leonardovee <leoveeph@gmail.com>
2021-04-12 21:55:23 -0300

    refactor: move main layer folders

--- src/main/factories/controllers/auth/login/login-controller-factory.ts
+++ src/main/factories/controllers/auth/login/login-controller-factory.ts
@@ -1,5 +1,5 @@
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/authentication/db-authentication-factory'
 import { LoginController } from '../../../../../presentation/controllers/authentication/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
 import { Controller } from '../../../../../presentation/protocols'
--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,6 +1,6 @@
 import { AddSurveyController } from '../../../../../presentation/controllers/survey/add-survey/add-survey-controller'
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
-import { makeDbAddSurvey } from '../../../usecases/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '../../../usecases/survey/add-survey/db-add-survey-factory'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
 import { Controller } from '../../../../../presentation/protocols'
 
